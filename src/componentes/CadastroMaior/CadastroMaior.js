2b3cadcab7d44addafb50948e3cdff4717c07be6
Vanessa da Paixão <vanessadapaixaobarros1@gmail.com>
2019-11-07 14:33:44 -0300

    Adicionar estrutura de Cadastro

---
+++ src/componentes/CadastroMaior/CadastroMaior.js
@@ -0,0 +1,16 @@
+import React, {useState} from "react";
+import Input from "../Input/Input";
+
+import "./CadastroMaior.scss";
+
+const CadastroMaior = () => {
+  
+  return (
+    <div className="CadastroMaior">
+       
+    
+    </div>
+    );
+  }
+
+export default CadastroMaior;

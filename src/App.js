5352192e508596002b9f379547969459bf4cc6b9
matheus s. antonio <viciogamer157@gmail.com>
2019-11-07 16:39:28 -0300

    iniciar tela de HorariosDosCursos

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { BrowserRouter, Route} from 'react-router-dom';
 import Navbar from './componentes/Navbar/Navbar.js';
 import Home from './componentes/Home/Home.js';
-import Cadastro from './componentes/CadastroMaior/CadastroMaior.js'
+import Cadastro from './componentes/CadastroMaior/CadastroMaior.js';
+import HoraiosDosCursos from './componentes/HorariosDosCursos.js';
 
 
 
@@ -12,7 +13,7 @@ const App = () => {
       <Navbar />
       <Route exact path="/" component={Home} />
       <Route path="/cadastro" component={Cadastro} />
-      
+      <Route path="/sobre" component={HorariosDosCursos} />
       
     </BrowserRouter>
   )

699cfde584ce0964bc7e62ecd2d8dac180243f3b
matheus s. antonio <viciogamer157@gmail.com>
2019-11-08 17:08:02 -0300

    adicionar turma 2 balé

--- src/componentes/DadosCursos/DadosCursos.js
+++ src/componentes/DadosCursos/DadosCursos.js
@@ -14,6 +14,14 @@ const DadosCursos = () => {
                     <p>Faixa etária: 3 a 6 anos/Baby</p>
                     <p>Local: CECIN</p>
                     <button>INSCREVA-SE</button>
+                    <div className="Linha"></div>
+                    <h2>TURMA 02</h2>
+                    <p>Dia: Quinta-feira</p>
+                    <p>Horário: 14:00 às 15:00</p>
+                    <p>Faixa etária: 12 a 17 anos/Juvenil</p>
+                    <p>Local: CECIN</p>
+                    <button>INSCREVA-SE</button> 
+            
                     
             </div>
             

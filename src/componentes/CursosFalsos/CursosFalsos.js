453d82239bc62c1fe0b0f2f66d730adce7948e1a
Luanamgs <Luanamgs10@gmail.com>
2019-11-11 16:47:54 -0300

    Adicionar CSS do container de cursos futuros

--- src/componentes/CursosFalsos/CursosFalsos.js
+++ src/componentes/CursosFalsos/CursosFalsos.js
@@ -6,13 +6,13 @@ const CursosContainerF = ({ img, titulo, texto, embreve }) => {
     return (
         <div className="CaixaCursos">
 
-            <div className="CursosD">
+            <div className="CursosF">
                 <img src={img} alt="" />
                 <h2>{titulo}</h2>
                 <p>{texto}</p>
                 <div id="botaofalso" ><p>{embreve}</p></div>
 
-                <div id="Risco"></div>
+                <div id="Risco1"></div>
 
             </div>
 

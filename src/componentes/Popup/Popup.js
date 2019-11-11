c93a1276e22cc8971d492eb7acec709504d169e3
vanessadapaixaobarros1@gmail.com <viciogamer157@gmail.com>
2019-11-11 16:08:54 -0300

    Adicionar CSS do popup

--- src/componentes/Popup/Popup.js
+++ src/componentes/Popup/Popup.js
@@ -8,11 +8,14 @@ const Popup = () => {
         <div className="fundo">
             <div className="popup">
                     <h1>Você foi cadastrado com sucesso!</h1>
-                    <p> Observação: O inscrito/a terá o prazo de 5 dias para comparecer até o local de 
-
+                    <p> <span>Atenção: </span>O inscrito/a (caso seja de menor levar responsável portando documento) terá o prazo de 5 dias para comparecer 
+                        até o local citado portando os seguintes documentos para concluir a inscrição:
+                        <p>*Copia do RG</p>
+                        <p>*Copia do CPF</p>
+                        <p>*Copia do comprovante de residência</p>
+                        <p>*1 foto 3X4</p>
                     </p>
-
-                  <Link to="/"><button>OK</button></Link>  
+                        <Link to="/"><button>OK</button></Link>  
             </div>
         </div>
 
